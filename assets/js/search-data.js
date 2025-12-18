7f23f820e95316a0a72200ce792644dcc24fa768
akmbr <105453976+akmbr@users.noreply.github.com>
2025-12-18 00:59:07 +0000

    Deploying to gh-pages from @ akmbr/akmbr.github.io@f09d70cb98a0af955c9c1c498fce82c11ae85c61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-me",
+    title: "me",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
