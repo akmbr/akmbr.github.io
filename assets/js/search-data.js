17915af7abe9939cdc998af9482df854b3550c01
akmbr <105453976+akmbr@users.noreply.github.com>
2025-12-18 01:18:50 +0000

    Deploying to gh-pages from @ akmbr/akmbr.github.io@6f72c1ee4967019c8511a9bc29233025d996cdaa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,28 +459,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%6B%61%6D%62%65%72@%75%63%73%64.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/akmbr", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/anlkamber", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=Rmq3l7gAAAAJ&hl", "_blank");
         },
       },{
         id: 'social-custom_social',
